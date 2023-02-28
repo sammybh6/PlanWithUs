3e9ad8e77ba8143366d344bc6be10c33a9db5a6c
sammybh6 <samyakbhargava9@gmail.com>
2023-02-28 14:11:56 +0530

    c2

--- Frontend/components/test.jsx
+++ Frontend/components/test.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+// const axios = require("axios");
+export default function test()
+{
 
-export default function test() {
-  return (
-    <div>test</div>
-  )
+    
+
+const options = {
+  method: 'GET',
+  url: 'https://irctc1.p.rapidapi.com/api/v2/trainBetweenStations',
+  params: {fromStationCode: 'jbp', toStationCode: 'pune'},
+  headers: {
+    'X-RapidAPI-Key': 'ba35c709c4msh1cddd9faeb06c26p1d8858jsnb908a9ad561a',
+    'X-RapidAPI-Host': 'irctc1.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+    return <div>test</div>;
 }
